31727abb1f97f46f8599632201bcb137260cef90
yntcdd <michaelzhang935@gmail.com>
2026-08-16 22:50:54 -0400

    Ai

--- game.py
+++ game.py
@@ -127,7 +127,6 @@ PLAYER5_AI = create_ai("gemini_str")   # blue STR
 PLAYER7_AI = create_ai("gemini_def")   # blue DEF1
 PLAYER9_AI = create_ai("gemini_def")   # blue DEF2
 
-
 # red team (attacks left) — Custom / Original AI mix
 PLAYER2_AI = create_ai("goalkeeper")   # red GK
 PLAYER4_AI = create_ai("playmaker")    # red PM
